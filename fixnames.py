e2182c4097c7e5af0d04e35d689c41e303f471cd
thomasgigout <thomas.gigout@thalesgroup.com>
2022-02-07 17:03:36 +0100

    :sparkles:

--- fixnames.py
+++ fixnames.py
@@ -2,15 +2,11 @@
 import os
 import sys
 import argparse
-import logging
 import glob
 import time
 import shutil
 
-logging.basicConfig(format='[%(levelname)s] %(message)s',level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(description="Fix the first number of a YOLO annotation file according to ancient / new names. For example: if 0 is 'gun' in new names, and 0 is 'handgun' in old, nothing change. But, if 0 is 'gun' in new names and 'gun' is 5 in the old names, 5 -> 0 in all text YOLO annotations files.",usage='use "%(prog)s --old-names path/to/old_classes.txt --new-names path/to/new_classes.txt --input path/to/folder/train/"', formatter_class=argparse.RawTextHelpFormatter)
-
 parser.add_argument("-o", "--old", help="File with 'old' names", type=str, required=True)
 parser.add_argument("-n","--new", help="File with 'new' names", type=str, required=True)
 parser.add_argument("-i", "--input", help="Path to text files to fix", type=str, required=True)
@@ -25,76 +21,52 @@ changes = {}
 	
 def fix(file):
 	global old_names, new_names
-	
-	#logger.info("Fixing file: "+file)
+
 	with open(file, 'r') as f:
 		lines = f.readlines()
-		filename = file.split('/')[-1]
-		output = args.Output+'/'+filename
+		if 'nt' in os.name:
+			sep = '\\'
+		else:
+			sep = '/'
+		filename = file.split(sep)[-1]
+		output = args.Output + sep + filename
+		print(filename, output)
 		with open(output, "w+") as o:
 			for line in lines:
-				splitted = line.split(" ")
-				#print(splitted)
-				if (
-					splitted[0] == str(d_old["Grinder"]) or 
-					splitted[0] == str(d_old["Screwdriver"]) or 
-					splitted[0] == str(d_old["Wrench"]) or 
-					splitted[0] == str(d_old["Grinder"]) or 
-					splitted[0] == str(d_old["Drill (Tool)"]) or 
-					splitted[0] == str(d_old["Bow and arrow"]) or 
-					splitted[0] == str(d_old["Ratchet (Device)"])
-				):
-					print(splitted[0] +" changed to "+str(d_new["other"]))
-					splitted[0] = str(d_new["other"])
-					line = " ".join(splitted)
-					
-				elif splitted[0] == str(d_old["Hammer"]):
-					print(splitted[0] +" changed to "+str(d_new["axe"]))
-					splitted[0] = str(d_new["axe"])
-					line = " ".join(splitted)
-					
-				elif splitted[0] == str(d_old["Kitchen knife"]):
-					print(splitted[0] +" changed to "+str(d_new["knife"]))
-					splitted[0] = str(d_new["knife"])
-					line = " ".join(splitted)
-					
-				elif splitted[0] == str(d_old["Shotgun"]):
-					print(splitted[0] +" changed to "+str(d_new["rifle"]))
-					splitted[0] = str(d_new["rifle"])
-					line = " ".join(splitted)
-				else:
-					print("Not changed")
-				o.write(line)
+				splitted = line.split()
+				splitted[0] = changes[int(splitted[0])]
+				line = ' '.join(splitted)
+				o.write(line + "\n")
 
 
-def logInfos():
-	logger.info("Old names foud : ")
-	logger.info(old_names)
-	logger.info("New names foud : ")
-	logger.info(new_names)
+def log_infos():
+	print("Old names found : ")
+	print(old_names)
+	print("New names found : ")
+	print(new_names)
+	print("Old: ")
 	print(d_old)
+	print("New: ")
 	print(d_new)
 
 
 def main():
 	global old_names, new_names
 	if not os.path.isfile(args.old):
-		logger.error("Argument -o not a file or does not exist.")
+		print("Argument -o not a file or does not exist.")
 		sys.exit(2)
 	if not os.path.isfile(args.new):
-		logger.error("Argument -n not a file or does not exist.")
+		print("Argument -n not a file or does not exist.")
 		sys.exit(2)
 	if not os.path.isdir(args.input):
-		logger.error("Argument -i not a folder or does not exist.")
+		print("Argument -i not a folder or does not exist.")
 		sys.exit(2)
 	
 	with open(args.old,'r') as o:
-		lines = o.readlines()
-		for line in lines:
+		for line in o.readlines():
 			old_names.append(line.strip('\n'))
 	with open(args.new,'r') as n:
-		lines = n.readlines()
-		for line in lines:
+		for line in n.readlines():
 			new_names.append(line.strip('\n'))
 	
 	cpt = 0
@@ -111,14 +83,15 @@ def main():
 
 	for el in d_old:
 		ans = -1
-		while not int(ans) in list(d_new.values()):
+		while ans == '' or not int(ans) in list(d_new.values()):
 			ask = f"Change \'{el}\' to which ID ?"
 			ans = input(f"{ask}\n{st} : ")
-		print("ok")
+		changes[d_old[el]] = ans
 
-	# for file in glob.glob(args.input+"/*.txt"):
-	# 	fix(file)
+	for file in glob.glob(args.input+"/*.txt"):
+		if "classes.txt" in file:
+			continue
+		fix(file)
 
 if __name__ == '__main__':
 	main()
-
